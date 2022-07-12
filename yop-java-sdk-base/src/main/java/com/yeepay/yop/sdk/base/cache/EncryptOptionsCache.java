6f5f106aed04db0375d89dea308a32d837f89e97
wdc <dachong.wang@yeepay.com>
2022-07-12 11:20:33 +0800

    feat: 加密选项添加刷新入口

--- yop-java-sdk-base/src/main/java/com/yeepay/yop/sdk/base/cache/EncryptOptionsCache.java
+++ yop-java-sdk-base/src/main/java/com/yeepay/yop/sdk/base/cache/EncryptOptionsCache.java
@@ -10,7 +10,9 @@ import com.google.common.cache.LoadingCache;
 import com.yeepay.yop.sdk.base.security.encrypt.Sm2Enhancer;
 import com.yeepay.yop.sdk.base.security.encrypt.YopEncryptorFactory;
 import com.yeepay.yop.sdk.exception.YopClientException;
-import com.yeepay.yop.sdk.security.encrypt.*;
+import com.yeepay.yop.sdk.security.encrypt.EncryptOptions;
+import com.yeepay.yop.sdk.security.encrypt.EncryptOptionsEnhancer;
+import com.yeepay.yop.sdk.security.encrypt.YopEncryptor;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,10 +51,63 @@ public class EncryptOptionsCache {
      */
     public static Future<EncryptOptions> loadEncryptOptions(String appKey, String encryptAlg) {
         try {
-            return ENCRYPT_OPTIONS_CACHE.get(StringUtils.joinWith(COMMA, appKey, encryptAlg));
+            return ENCRYPT_OPTIONS_CACHE.get(getCacheKey(appKey, encryptAlg));
         } catch (ExecutionException e) {
             throw new YopClientException("initEncryptOptions error, ex:", e);
         }
+    }
+
+    /**
+     * 刷新加密选项，并缓存（异步操作，不立即生效）
+     *
+     * Loads a new value for key, possibly asynchronously.
+     * While the new value is loading the previous value (if any) will continue to be returned by get(key) unless it is evicted.
+     * If the new value is loaded successfully it will replace the previous value in the cache;
+     * if an exception is thrown while refreshing the previous value will remain, and the exception will be logged (using java.util.logging.Logger) and swallowed
+     *
+     * @param appKey     应用
+     * @param encryptAlg 加解密算法
+     */
+    public static void refreshEncryptOptions(String appKey, String encryptAlg) {
+        try {
+            ENCRYPT_OPTIONS_CACHE.refresh(getCacheKey(appKey, encryptAlg));
+        } catch (Exception e) {
+            throw new YopClientException("refreshEncryptOptions error, ex:", e);
+        }
+    }
+
+    /**
+     * 立即失效加密选项
+     *
+     * @param appKey     应用
+     * @param encryptAlg 加解密算法
+     */
+    public static void invalidateEncryptOptions(String appKey, String encryptAlg) {
+        try {
+            ENCRYPT_OPTIONS_CACHE.invalidate(getCacheKey(appKey, encryptAlg));
+        } catch (Exception e) {
+            throw new YopClientException("invalidateEncryptOptions error, ex:", e);
+        }
+    }
+
+    /**
+     * 失效并同步加载新的加密选项，并缓存
+     *
+     * @param appKey 应用
+     * @param encryptAlg 加解密算法
+     * @return Future<EncryptOptions>
+     */
+    public static Future<EncryptOptions> reloadEncryptOptions(String appKey, String encryptAlg) {
+        try {
+            ENCRYPT_OPTIONS_CACHE.invalidate(getCacheKey(appKey, encryptAlg));
+        } catch (Exception e) {
+            throw new YopClientException("invalidate EncryptOptions error, ex:", e);
+        }
+        return loadEncryptOptions(appKey, encryptAlg);
+    }
+
+    private static String getCacheKey(String appKey, String encryptAlg) {
+        return StringUtils.joinWith(COMMA, appKey, encryptAlg);
     }
 
     private static LoadingCache<String, Future<EncryptOptions>> initCache(Long expire, TimeUnit timeUnit) {
