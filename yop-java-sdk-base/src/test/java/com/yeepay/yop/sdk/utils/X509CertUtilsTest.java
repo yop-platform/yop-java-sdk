39c36cd005aefa1b796d8e3e165451016592439c
wdc <dachong.wang@yeepay.com>
2022-07-20 11:03:34 +0800

    feat: import refactor

--- yop-java-sdk-base/src/test/java/com/yeepay/yop/sdk/utils/X509CertUtilsTest.java
+++ yop-java-sdk-base/src/test/java/com/yeepay/yop/sdk/utils/X509CertUtilsTest.java
@@ -4,10 +4,9 @@
  */
 package com.yeepay.yop.sdk.utils;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * title: X509CertUtilsTest<br>
@@ -23,21 +22,21 @@ public class X509CertUtilsTest {
 
     @Test
     public void parseToHex() {
-        Assert.assertEquals(X509CertUtils.parseToHex("275718169735"), "4032156487");
-        Assert.assertEquals(X509CertUtils.parseToHex("290297451928"), "4397139598");
-        Assert.assertEquals(X509CertUtils.parseToHex("275550212193"), "4028129061");
-        Assert.assertEquals(X509CertUtils.parseToHex("275568425014"), "4029287836");
-        Assert.assertEquals(X509CertUtils.parseToHex("289782695477"), "4378650635");
-        Assert.assertEquals(X509CertUtils.parseToHex("289798445125"), "4379555845");
+        assertEquals(X509CertUtils.parseToHex("275718169735"), "4032156487");
+        assertEquals(X509CertUtils.parseToHex("290297451928"), "4397139598");
+        assertEquals(X509CertUtils.parseToHex("275550212193"), "4028129061");
+        assertEquals(X509CertUtils.parseToHex("275568425014"), "4029287836");
+        assertEquals(X509CertUtils.parseToHex("289782695477"), "4378650635");
+        assertEquals(X509CertUtils.parseToHex("289798445125"), "4379555845");
     }
 
     @Test
     public void parseToDecimal() {
-        Assert.assertEquals(X509CertUtils.parseToDecimal("4032156487"), "275718169735");
-        Assert.assertEquals(X509CertUtils.parseToDecimal("4397139598"), "290297451928");
-        Assert.assertEquals(X509CertUtils.parseToDecimal("4028129061"), "275550212193");
-        Assert.assertEquals(X509CertUtils.parseToDecimal("4029287836"), "275568425014");
-        Assert.assertEquals(X509CertUtils.parseToDecimal("4378650635"), "289782695477");
-        Assert.assertEquals(X509CertUtils.parseToDecimal("4379555845"), "289798445125");
+        assertEquals(X509CertUtils.parseToDecimal("4032156487"), "275718169735");
+        assertEquals(X509CertUtils.parseToDecimal("4397139598"), "290297451928");
+        assertEquals(X509CertUtils.parseToDecimal("4028129061"), "275550212193");
+        assertEquals(X509CertUtils.parseToDecimal("4029287836"), "275568425014");
+        assertEquals(X509CertUtils.parseToDecimal("4378650635"), "289782695477");
+        assertEquals(X509CertUtils.parseToDecimal("4379555845"), "289798445125");
     }
 }
