b57993fb107801275c156115cc5697228ee6274e
wdc <dachong.wang@yeepay.com>
2023-03-21 14:12:36 +0800

    feat: 商户通知解析默认字符集指定UTF-8

--- src/main/java/com/yeepay/yop/sdk/utils/Sm4Utils.java
+++ src/main/java/com/yeepay/yop/sdk/utils/Sm4Utils.java
@@ -4,6 +4,7 @@
  */
 package com.yeepay.yop.sdk.utils;
 
+import com.google.common.base.Charsets;
 import com.yeepay.yop.sdk.exception.YopClientException;
 import org.apache.commons.lang3.StringUtils;
 import org.bouncycastle.crypto.CipherParameters;
@@ -113,7 +114,7 @@ public class Sm4Utils {
         GCMParameterSpec spec = new GCMParameterSpec(DEFAULT_KEY_SIZE, nonceBytes);
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, spec);
         if (StringUtils.isNotBlank(associatedData)) {
-            cipher.updateAAD(associatedData.getBytes());
+            cipher.updateAAD(associatedData.getBytes(Charsets.UTF_8));
         }
         return cipher.doFinal(Encodes.decodeBase64(ciphertext));
     }
