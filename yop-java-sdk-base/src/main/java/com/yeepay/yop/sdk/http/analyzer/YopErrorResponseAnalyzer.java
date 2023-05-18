0ba9a2072f3ac2344de3909c53bdd62bad684cc9
wdc <dachong.wang@yeepay.com>
2023-05-18 20:04:20 +0800

    refactor: 补充计数异常码(404、502)

--- yop-java-sdk-base/src/main/java/com/yeepay/yop/sdk/http/analyzer/YopErrorResponseAnalyzer.java
+++ yop-java-sdk-base/src/main/java/com/yeepay/yop/sdk/http/analyzer/YopErrorResponseAnalyzer.java
@@ -1,11 +1,12 @@
 package com.yeepay.yop.sdk.http.analyzer;
 
 import com.yeepay.yop.sdk.exception.YopClientException;
+import com.yeepay.yop.sdk.exception.YopHttpException;
 import com.yeepay.yop.sdk.exception.YopServiceException;
+import com.yeepay.yop.sdk.http.HttpResponseAnalyzer;
 import com.yeepay.yop.sdk.http.HttpResponseHandleContext;
 import com.yeepay.yop.sdk.http.HttpStatus;
 import com.yeepay.yop.sdk.http.YopHttpResponse;
-import com.yeepay.yop.sdk.http.HttpResponseAnalyzer;
 import com.yeepay.yop.sdk.model.BaseResponse;
 import com.yeepay.yop.sdk.model.YopErrorResponse;
 import com.yeepay.yop.sdk.utils.JsonUtils;
@@ -39,23 +40,16 @@ public class YopErrorResponseAnalyzer implements HttpResponseAnalyzer {
     public <T extends BaseResponse> boolean analysis(HttpResponseHandleContext context, T response) throws Exception {
         YopHttpResponse httpResponse = context.getResponse();
         int statusCode = httpResponse.getStatusCode();
+        // 2xx
         if (statusCode / 100 == HttpStatus.SC_OK / 100 && statusCode != HttpStatus.SC_NO_CONTENT) {
             // not an error
             return false;
         }
+        // 5xx
         if (statusCode >= HttpStatus.SC_INTERNAL_SERVER_ERROR && statusCode != HttpStatus.SC_BAD_GATEWAY) {
             YopServiceException yse = null;
             String content = httpResponse.readContent();
-            if (content != null) {
-                /*
-                 * content-length is not set in the error respond message of the media service
-                 */
-//            if (response.getMetadata().getContentLength() > 0) {
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
-//            String line;
-//            while ((line = bufferedReader.readLine()) != null){
-//                System.out.println(line);
-//            }
+            if (null != content) {
                 YopErrorResponse yopErrorResponse = null;
                 try {
                     yopErrorResponse = JsonUtils.loadFrom(content, YopErrorResponse.class);
@@ -76,13 +70,11 @@ public class YopErrorResponseAnalyzer implements HttpResponseAnalyzer {
                 yse.setRequestId(response.getMetadata().getYopRequestId());
             }
             yse.setStatusCode(httpResponse.getStatusCode());
-            if (yse.getStatusCode() >= 500) {
-                yse.setErrorType(YopServiceException.ErrorType.Service);
-            } else {
-                yse.setErrorType(YopServiceException.ErrorType.Client);
-            }
+            yse.setErrorType(YopServiceException.ErrorType.Service);
             throw yse;
-        } else {
+        } else if (statusCode == HttpStatus.SC_BAD_GATEWAY || statusCode == HttpStatus.SC_NOT_FOUND) {
+            throw new YopHttpException("Response Error, statusCode:" + statusCode);
+        } else {// 4xx
             throw new YopClientException("unexpected httpStatusCode:" + statusCode);
         }
     }
