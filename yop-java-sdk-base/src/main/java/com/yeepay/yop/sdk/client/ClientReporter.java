2233b8915e36f5d7903631e59463d9f45d83a984
wdc <dachong.wang@yeepay.com>
2023-06-05 17:38:47 +0800

    refactor: 巧用线程池拒绝过期事件

--- yop-java-sdk-base/src/main/java/com/yeepay/yop/sdk/client/ClientReporter.java
+++ yop-java-sdk-base/src/main/java/com/yeepay/yop/sdk/client/ClientReporter.java
@@ -5,6 +5,7 @@
 package com.yeepay.yop.sdk.client;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Queues;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.yeepay.yop.sdk.base.config.provider.YopSdkConfigProviderRegistry;
 import com.yeepay.yop.sdk.client.metric.event.api.YopHostRequestEvent;
@@ -37,19 +38,20 @@ import java.util.concurrent.atomic.AtomicReference;
  * @version 1.0.0
  * @since 2023/3/29
  */
-public class ClientReporter extends Thread {
+public class ClientReporter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ClientReporter.class);
 
     private static final YopReporter DEFAULT_REPORTER = YopRemoteReporter.INSTANCE;
 
-    private static final LinkedBlockingQueue<YopHostRequestEvent<?>> YOP_HOST_REQUEST_EVENT_QUEUE;
-
     private static final ConcurrentMap<String, AtomicReference<YopHostRequestReport>> YOP_HOST_REQUEST_REPORTS = new ConcurrentHashMap<>();
+
+    // 打包上报事件
+    private static final ThreadPoolExecutor COLLECT_POOL;
+
+    // 将打包后的事件发送到远端
     private static final ScheduledThreadPoolExecutor SEND_SCHEDULE_POOL = new ScheduledThreadPoolExecutor(1,
             new ThreadFactoryBuilder().setNameFormat("client-report-sender-%d").setDaemon(true).build());
-
-    private static final ClientReporter REPORTER_PROXY = new ClientReporter();
 
     private static final int REPORT_INTERVAL_MILLISECONDS;
     private static final int REPORT_MIN_INTERVAL_MILLISECONDS;
@@ -96,10 +98,9 @@ public class ClientReporter extends Thread {
             MAX_FAIL_COUNT_PER_EX = 5;
             MAX_ELAPSED_MILLISECONDS = 15000;
         }
-        YOP_HOST_REQUEST_EVENT_QUEUE = new LinkedBlockingQueue<>(MAX_QUEUE_SIZE);
-        REPORTER_PROXY.setName("ClientReporter");
-        REPORTER_PROXY.setDaemon(true);
-        REPORTER_PROXY.start();
+        COLLECT_POOL = new ThreadPoolExecutor(1, 1,
+                30, TimeUnit.SECONDS, Queues.newLinkedBlockingQueue(MAX_QUEUE_SIZE),
+                new ThreadFactoryBuilder().setNameFormat("client-report-sender-%d").setDaemon(true).build(), new ThreadPoolExecutor.DiscardOldestPolicy());
         SEND_SCHEDULE_POOL.scheduleWithFixedDelay(new Runnable() {
             public void run() {
                 try {
@@ -115,111 +116,111 @@ public class ClientReporter extends Thread {
         try {
             if (!REPORT) {
                 if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("Ignore Report Event, value:{}", newEvent);
+                    LOGGER.debug("Ignore ReportEvent, value:{}", newEvent);
                 }
                 return;
             }
             if (!REPORT_SUCCESS && YopHostRequestStatus.SUCCESS.equals(newEvent.getStatus())) {
                 if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("Ignore Success Report Event, value:{}", newEvent);
+                    LOGGER.debug("Ignore Success ReportEvent, value:{}", newEvent);
                 }
                 return;
             }
-            while (!YOP_HOST_REQUEST_EVENT_QUEUE.offer(newEvent)) {
-                YopHostRequestEvent<?> oldEvent = YOP_HOST_REQUEST_EVENT_QUEUE.poll();
-                if (oldEvent != null) {
-                    LOGGER.info("Discard Old Event, value:{}", oldEvent);
-                }
-            }
+            collectEvents(newEvent);
         } catch (Exception exception) {
             LOGGER.error("Error Handle ReportEvent, value:" + newEvent, exception);
         }
     }
 
-    @Override
-    public void run() {
-        while (!CLOSED) {
-            try {
-                aggregateEvents();
-            } catch (Exception e) {
-                LOGGER.warn("Error Happened, ex:", e);
-            }
-        }
-    }
-
-    private static void aggregateEvents() {
-        final YopHostRequestEvent<?> event = YOP_HOST_REQUEST_EVENT_QUEUE.poll();
+    private static void collectEvents(YopHostRequestEvent<?> event) {
         if (null == event) return;
-
-        final String serverHost = event.getServerHost();
-        final String serverIp = event.getServerIp();
-        final long elapsedMillis = event.getElapsedMillis();
-        int successCount = 0;
-        int failCount = 0;
-        YopFailDetailItem failDetail = null;
-        if (YopHostRequestStatus.SUCCESS.equals(event.getStatus())) {
-            successCount = 1;
-        } else {
-            failCount = 1;
-            failDetail = (YopFailDetailItem) event.getData();
-        }
-
-        final String reportKey = serverHost + serverIp;
-        AtomicReference<YopHostRequestReport> reportReference = YOP_HOST_REQUEST_REPORTS.get(reportKey);
-
-        if (null == reportReference) {
-            YOP_HOST_REQUEST_REPORTS.putIfAbsent(reportKey, new AtomicReference<>());
-            reportReference = YOP_HOST_REQUEST_REPORTS.get(reportKey);
-        }
-
-        // CompareAndSet并发加入统计数据
-        YopHostRequestReport current;
-        YopHostRequestReport update = new YopHostRequestReport();
-        do {
-            current = reportReference.get();
-            if (current == null) {
-                YopHostRequestPayload payload = new YopHostRequestPayload();
-                update.setPayload(payload);
-                payload.setServerIp(serverIp);
-                payload.setServerHost(serverHost);
-                payload.setSuccessCount(successCount);
-                payload.setFailCount(failCount);
-                payload.setMaxElapsedMillis(elapsedMillis);
-                payload.setFailDetails(Lists.newLinkedList());
-                if (null != failDetail) {
-                    final YopFailDetail yopFailDetail = new YopFailDetail(failDetail.getExType(), failDetail.getExMsg());
-                    yopFailDetail.getOccurTime().add(failDetail.getOccurTime());
-                    payload.getFailDetails().add(yopFailDetail);
-                }
-            } else {
-                YopHostRequestPayload oldPayload = current.getPayload();
-                YopHostRequestPayload payload = new YopHostRequestPayload();
-                update.setPayload(payload);
-                payload.setServerIp(serverIp);
-                payload.setServerHost(serverHost);
-                payload.setSuccessCount(oldPayload.getSuccessCount() + successCount);
-                payload.setFailCount(oldPayload.getFailCount() + failCount);
-                payload.setMaxElapsedMillis(Math.max(elapsedMillis, oldPayload.getMaxElapsedMillis()));
-                payload.setFailDetails(Lists.newLinkedList(oldPayload.getFailDetails()));
-                final YopFailDetailItem failDetailItem = failDetail;
-                if (null != failDetail) {
-                    final Optional<YopFailDetail> yopFailDetail = payload.getFailDetails().stream().filter(p ->
-                            StringUtils.equals(p.getExType(), failDetailItem.getExType())
-                                    && StringUtils.equals(p.getExMsg(), failDetailItem.getExMsg()))
-                            .findAny();
-                    if (yopFailDetail.isPresent()) {
-                        yopFailDetail.get().getOccurTime().add(failDetailItem.getOccurTime());
-                    } else {
-                        final YopFailDetail newYopFailDetail = new YopFailDetail(failDetail.getExType(), failDetail.getExMsg());
-                        newYopFailDetail.getOccurTime().add(failDetail.getOccurTime());
-                        payload.getFailDetails().add(newYopFailDetail);
-                    }
-                }
-            }
-        } while (!reportReference.compareAndSet(current, update));
+        if (CLOSED) return;
+        COLLECT_POOL.submit(new CollectTask(event));
     }
 
-    private static void sendReport() {
+    private static class CollectTask implements Runnable {
+        private final YopHostRequestEvent<?> event;
+
+        public CollectTask(YopHostRequestEvent<?> event) {
+            this.event = event;
+        }
+
+        @Override
+        public void run() {
+            try {
+                final String serverHost = event.getServerHost();
+                final String serverIp = event.getServerIp();
+                final long elapsedMillis = event.getElapsedMillis();
+                int successCount = 0;
+                int failCount = 0;
+                YopFailDetailItem failDetail = null;
+                if (YopHostRequestStatus.SUCCESS.equals(event.getStatus())) {
+                    successCount = 1;
+                } else {
+                    failCount = 1;
+                    failDetail = (YopFailDetailItem) event.getData();
+                }
+
+                final String reportKey = serverHost + serverIp;
+                AtomicReference<YopHostRequestReport> reportReference = YOP_HOST_REQUEST_REPORTS.get(reportKey);
+
+                if (null == reportReference) {
+                    YOP_HOST_REQUEST_REPORTS.putIfAbsent(reportKey, new AtomicReference<>());
+                    reportReference = YOP_HOST_REQUEST_REPORTS.get(reportKey);
+                }
+
+                // CompareAndSet并发加入统计数据
+                YopHostRequestReport current;
+                YopHostRequestReport update = new YopHostRequestReport();
+                do {
+                    current = reportReference.get();
+                    if (current == null) {
+                        YopHostRequestPayload payload = new YopHostRequestPayload();
+                        update.setPayload(payload);
+                        payload.setServerIp(serverIp);
+                        payload.setServerHost(serverHost);
+                        payload.setSuccessCount(successCount);
+                        payload.setFailCount(failCount);
+                        payload.setMaxElapsedMillis(elapsedMillis);
+                        payload.setFailDetails(Lists.newLinkedList());
+                        if (null != failDetail) {
+                            final YopFailDetail yopFailDetail = new YopFailDetail(failDetail.getExType(), failDetail.getExMsg());
+                            yopFailDetail.getOccurTime().add(failDetail.getOccurTime());
+                            payload.getFailDetails().add(yopFailDetail);
+                        }
+                    } else {
+                        YopHostRequestPayload oldPayload = current.getPayload();
+                        YopHostRequestPayload payload = new YopHostRequestPayload();
+                        update.setPayload(payload);
+                        payload.setServerIp(serverIp);
+                        payload.setServerHost(serverHost);
+                        payload.setSuccessCount(oldPayload.getSuccessCount() + successCount);
+                        payload.setFailCount(oldPayload.getFailCount() + failCount);
+                        payload.setMaxElapsedMillis(Math.max(elapsedMillis, oldPayload.getMaxElapsedMillis()));
+                        payload.setFailDetails(Lists.newLinkedList(oldPayload.getFailDetails()));
+                        final YopFailDetailItem failDetailItem = failDetail;
+                        if (null != failDetail) {
+                            final Optional<YopFailDetail> yopFailDetail = payload.getFailDetails().stream().filter(p ->
+                                            StringUtils.equals(p.getExType(), failDetailItem.getExType())
+                                                    && StringUtils.equals(p.getExMsg(), failDetailItem.getExMsg()))
+                                    .findAny();
+                            if (yopFailDetail.isPresent()) {
+                                yopFailDetail.get().getOccurTime().add(failDetailItem.getOccurTime());
+                            } else {
+                                final YopFailDetail newYopFailDetail = new YopFailDetail(failDetail.getExType(), failDetail.getExMsg());
+                                newYopFailDetail.getOccurTime().add(failDetail.getOccurTime());
+                                payload.getFailDetails().add(newYopFailDetail);
+                            }
+                        }
+                    }
+                } while (!reportReference.compareAndSet(current, update));
+            } catch (Exception e) {
+                LOGGER.warn("Error Collect ReportEvent, value:" + event, e);
+            }
+        }
+    }
+
+    private static void sendReport() throws InterruptedException {
         long timestamp = System.currentTimeMillis();
         List<YopReport> reports = Lists.newLinkedList();
         final Iterator<Map.Entry<String, AtomicReference<YopHostRequestReport>>> iterator =
@@ -241,14 +242,23 @@ public class ClientReporter extends Thread {
         if (CollectionUtils.isEmpty(reports)) {
             return;
         }
-        for (List<YopReport> reportsSending : Lists.partition(reports, MAX_PACKET_SIZE)) {
-            try {
-                DEFAULT_REPORTER.batchReport(reportsSending);
-                Thread.sleep(REPORT_MIN_INTERVAL_MILLISECONDS);//TODO
-            } catch (InterruptedException e) {
-                // ignore
+        final List<List<YopReport>> partitions = Lists.partition(reports, MAX_PACKET_SIZE);
+        if (partitions.size() == 1) {
+            doSendReport(reports);
+            return;
+        }
+
+        for (int i = 0, total = partitions.size(); i < total; i++) {
+            List<YopReport> reportsSending = partitions.get(i);
+            doSendReport(reportsSending);
+            if (i != total -1) {
+                Thread.sleep(REPORT_MIN_INTERVAL_MILLISECONDS);
             }
         }
+    }
+
+    private static void doSendReport(List<YopReport> reports) {
+        DEFAULT_REPORTER.batchReport(reports);
     }
 
     private static boolean needReport(long currentTime, YopHostRequestReport report) {
@@ -282,9 +292,19 @@ public class ClientReporter extends Thread {
     public static void close() {
         try {
             CLOSED = true;
-            REPORTER_PROXY.interrupt();
         } catch (Exception exception) {
             LOGGER.error("Error When Close ClientReporter", exception);
+        }
+    }
+
+    /**
+     * 开启上报器
+     */
+    public static void open() {
+        try {
+            CLOSED = false;
+        } catch (Exception exception) {
+            LOGGER.error("Error When Open ClientReporter", exception);
         }
     }
 
