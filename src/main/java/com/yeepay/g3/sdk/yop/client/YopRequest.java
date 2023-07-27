981d4694de03e85ae07f8b42429d04d31957ba14
wdc <dachong.wang@yeepay.com>
2023-07-27 12:05:18 +0800

    feat: sdk上传语言&版本

--- src/main/java/com/yeepay/g3/sdk/yop/client/YopRequest.java
+++ src/main/java/com/yeepay/g3/sdk/yop/client/YopRequest.java
@@ -113,6 +113,7 @@ public class YopRequest {
 
     private void init() {
         headers.put(Headers.YOP_SDK_VERSION, YopConstants.CLIENT_VERSION);
+        headers.put(Headers.YOP_SDK_LANGS, YopConstants.CLIENT_LANGS);
         headers.put(Headers.USER_AGENT, YopConstants.USER_AGENT);
         headers.put(Headers.YOP_APP_KEY, this.appSdkConfig.getAppKey());
     }
