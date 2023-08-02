b663163b9752426703395a1564a531926dd0ac57
wdc <dachong.wang@yeepay.com>
2023-08-02 13:13:13 +0800

    refactor: 指定credentials请求

--- src/main/java/com/yeepay/yop/sdk/auth/req/AuthorizationReqSupport.java
+++ src/main/java/com/yeepay/yop/sdk/auth/req/AuthorizationReqSupport.java
@@ -1,5 +1,6 @@
 package com.yeepay.yop.sdk.auth.req;
 
+import com.yeepay.yop.sdk.security.CertTypeEnum;
 import com.yeepay.yop.sdk.security.DigestAlgEnum;
 import com.yeepay.yop.sdk.security.SignerTypeEnum;
 
@@ -18,24 +19,33 @@ import java.util.Map;
  */
 public class AuthorizationReqSupport {
 
+    public static final String SECURITY_SM2 = "YOP-SM2-SM3", SECURITY_RSA2048 = "YOP-RSA2048-SHA256", SECURITY_OAUTH2 = "YOP-OAUTH2";
+
     private static final Map<String, AuthorizationReq> supportedAuthorizationReqs;
+
+    private static final Map<CertTypeEnum, AuthorizationReq> CERT_SUPPORTED_AUTH_REQS;
 
     static {
         supportedAuthorizationReqs = new HashMap<String, AuthorizationReq>();
-        supportedAuthorizationReqs.put("YOP-SM2-SM3",
-                AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.SM2)
-                        .withCredentialType("SM2")
-                        .withSignatureAlg("SM3withSM2")
-                        .withDigestAlg(DigestAlgEnum.SM3)
-                        .withProtocolPrefix("YOP-SM2-SM3")
-                        .build());
-        supportedAuthorizationReqs.put("YOP-RSA2048-SHA256",
-                AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.RSA)
-                        .withCredentialType("RSA2048")
-                        .withSignatureAlg("SHA256withRSA")
-                        .withDigestAlg(DigestAlgEnum.SHA256)
-                        .withProtocolPrefix("YOP-RSA2048-SHA256")
-                        .build());
+        CERT_SUPPORTED_AUTH_REQS = new HashMap<CertTypeEnum, AuthorizationReq>();
+        final AuthorizationReq sm2 = AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.SM2)
+                .withCredentialType("SM2")
+                .withSignatureAlg("SM3withSM2")
+                .withDigestAlg(DigestAlgEnum.SM3)
+                .withProtocolPrefix("YOP-SM2-SM3")
+                .build();
+        supportedAuthorizationReqs.put(SECURITY_SM2, sm2);
+        CERT_SUPPORTED_AUTH_REQS.put(CertTypeEnum.SM2, sm2);
+
+        final AuthorizationReq rsa2048 = AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.RSA)
+                .withCredentialType("RSA2048")
+                .withSignatureAlg("SHA256withRSA")
+                .withDigestAlg(DigestAlgEnum.SHA256)
+                .withProtocolPrefix("YOP-RSA2048-SHA256")
+                .build();
+        supportedAuthorizationReqs.put(SECURITY_RSA2048, rsa2048);
+        CERT_SUPPORTED_AUTH_REQS.put(CertTypeEnum.RSA2048, rsa2048);
+
         supportedAuthorizationReqs.put("YOP-RSA2048-SHA512",
                 AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.RSA)
                         .withCredentialType("RSA2048")
@@ -58,16 +68,21 @@ public class AuthorizationReqSupport {
                         .withDigestAlg(DigestAlgEnum.SHA512)
                         .withProtocolPrefix("YOP-RSA4096-SHA512")
                         .build());
-        supportedAuthorizationReqs.put("YOP-OAUTH2",
-                AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.OAUTH2)
-                        .withCredentialType("TOKEN")
-                        .withDigestAlg(DigestAlgEnum.SHA256)
-                        .withProtocolPrefix("Bearer")
-                        .build());
+
+        final AuthorizationReq oauth2 = AuthorizationReq.Builder.anAuthorizationReq().withSignerType(SignerTypeEnum.OAUTH2)
+                .withCredentialType("TOKEN")
+                .withDigestAlg(DigestAlgEnum.SHA256)
+                .withProtocolPrefix("Bearer")
+                .build();
+        supportedAuthorizationReqs.put(SECURITY_OAUTH2, oauth2);
     }
 
     public static AuthorizationReq getAuthorizationReq(String securityReq) {
         return supportedAuthorizationReqs.get(securityReq);
+    }
+
+    public static AuthorizationReq getAuthorizationReq(CertTypeEnum certTypeEnum) {
+        return CERT_SUPPORTED_AUTH_REQS.get(certTypeEnum);
     }
 
 }
