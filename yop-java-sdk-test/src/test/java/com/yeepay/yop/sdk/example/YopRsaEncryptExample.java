c63cdb0d679fccadef0be21707578bb5ae0cae1f
wdc <dachong.wang@yeepay.com>
2023-10-20 15:48:44 +0800

    feat: post json 示例

--- yop-java-sdk-test/src/test/java/com/yeepay/yop/sdk/example/YopRsaEncryptExample.java
+++ yop-java-sdk-test/src/test/java/com/yeepay/yop/sdk/example/YopRsaEncryptExample.java
@@ -17,6 +17,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.joda.time.DateTime;
@@ -51,12 +52,15 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class YopRsaEncryptExample {
 
     private static final CloseableHttpClient httpClient = HttpClients.createDefault();
-    private static final String appKey = "app_100800191870146";
-    private static final String isvPrivateKey = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCcl9jdReLN2vk57DWeu2l223gO6txH4ra/cOkM1rHYSwUqrvVYUM61qNL4AZNO3nlBMUkEtZ5RyvBGajJ0OZidenqcnXHKTBOD6GcVpBsS4qCU5hIHspI0khYBhikl1z74E3HHe9sIwVz+gEx2TkCwH9aH4jh68YeKk25+tHeb75JPMhdj0S+IzWyNHFOOTPD6VQ3vCq1RDqutL56gVBIt6p4+ZQ4GkTDRQoD7OKWDZOkzXz+JnaLqIN/hnZ/8ag+0WY60rgpjXBAX1ezZtHGp33L4sqnIZIzIp38e1Oyfe+Ab1cBQkaRaBqKYRXjq7NXG0sfeuoFxJONPQfjSsYcxAgMBAAECggEABAJAJ0QBfycer7ysD7z5AXWzYGBjVMTJTGPZ134Mjf63qmTRu5nP/OcORZKWwJNh88kM9z2mCK6DEa5ozcBmt4tZ5bYDInxpmHwj3XI2zjg2h7FPH1rTMtzViuLyHTmiL0wiIsr5K8N1e79xlarBrbCW96ITM5SI1YOaNcytbjS82Y0b6AS3Jx4H02ihcpzFRPYZNffLhE3lu4WN6aoiQUnWvC+agwF88osHmUIwrKlo9unwzjzOh2bXGSvzTESeGdDMEz4ekNRX8dEfC31sJwd2tUqha5hTev/oFgymNwFhQvj0OfnB1oKjf7gaRbOYjFf5P3nou5d4EBR7OyyTpQKBgQC6pXRqwpVh8Hhvf0/vFYZk5yQsKBi/ik4vyYtIZVtdZGIq4vjUpIftt9WoxTYKhjgOL0vsPvtbR79XnMFOb0PSZXARfMoUlAwtMM+mk9N3fb5a6jrgV617Ob1Oqf691WPqdprb6t1HMBSYTzD3J1mPKeGmItsTAlFZuUjzkyvN3QKBgQDWx52jTXvc4QyOqu4Fc0mPSDJEYBBB6XpnTXRIT8rUFD9SDTWRhb29UL2nrtoQ6/8ZTC+/wKQ5B9UAUEwNxSk8IsrWDhcuioxOc1rT7nPRoVXQarr35//NV1CPmZyn6ybpCqDTsy6mvtWLIJdjZCc+1yZevM1uvOtRMrWRhs8bZQKBgEqIpfu4JqVMvRtxUL9d7iQ/NW+4t2FN3rkwl8FaUGj0HEuaBdoMtgdVASp7ToBXZu0rL/twjzm9ZgibnYov3nqXbXBeT+h10oL9Wf7gS3MNMMXngYlzGeD6hsFyGzs9ir/niyHFIYY7Cg5kmV4pRZdpFyYcBzYJF+lnl11FaRm1AoGAFu1wMoKO+mE7ye8NQZ+w9o6qbwoiMicOXgCyrRV3fXQ73jJyyXoRayg3VrMfrDbFIJo1bq7N2Riw8DuiIsYtRLIiHP+cEefQWn+N7pnB21rxojICi3xEnlL30px/UJ2VpcLwsCisjjhI63UrM/z5A4hMHEjjVTLtm9lh8IsHiNECgYBf+YEmhG88617fUMa7p9VLcwZNXWOgAFX230CYBFQQIgDuwINASoM4GGVfnvLPym4+WNW3cvfI1sNSm3VXzoM+uSHZtQUkG0SjHZl3buNGXXaJ1iZK/EoRHGraukC6ZuaDsnnjFQbRqvdZIFXqEWFbPFSWBQm9g2dYSbzNu3u9Kg==";
+    private static final String appKey = "sandbox_rsa_10080041523";
+//    private static final String appKey = "app_100800191870146";
+//    private static final String isvPrivateKey = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCcl9jdReLN2vk57DWeu2l223gO6txH4ra/cOkM1rHYSwUqrvVYUM61qNL4AZNO3nlBMUkEtZ5RyvBGajJ0OZidenqcnXHKTBOD6GcVpBsS4qCU5hIHspI0khYBhikl1z74E3HHe9sIwVz+gEx2TkCwH9aH4jh68YeKk25+tHeb75JPMhdj0S+IzWyNHFOOTPD6VQ3vCq1RDqutL56gVBIt6p4+ZQ4GkTDRQoD7OKWDZOkzXz+JnaLqIN/hnZ/8ag+0WY60rgpjXBAX1ezZtHGp33L4sqnIZIzIp38e1Oyfe+Ab1cBQkaRaBqKYRXjq7NXG0sfeuoFxJONPQfjSsYcxAgMBAAECggEABAJAJ0QBfycer7ysD7z5AXWzYGBjVMTJTGPZ134Mjf63qmTRu5nP/OcORZKWwJNh88kM9z2mCK6DEa5ozcBmt4tZ5bYDInxpmHwj3XI2zjg2h7FPH1rTMtzViuLyHTmiL0wiIsr5K8N1e79xlarBrbCW96ITM5SI1YOaNcytbjS82Y0b6AS3Jx4H02ihcpzFRPYZNffLhE3lu4WN6aoiQUnWvC+agwF88osHmUIwrKlo9unwzjzOh2bXGSvzTESeGdDMEz4ekNRX8dEfC31sJwd2tUqha5hTev/oFgymNwFhQvj0OfnB1oKjf7gaRbOYjFf5P3nou5d4EBR7OyyTpQKBgQC6pXRqwpVh8Hhvf0/vFYZk5yQsKBi/ik4vyYtIZVtdZGIq4vjUpIftt9WoxTYKhjgOL0vsPvtbR79XnMFOb0PSZXARfMoUlAwtMM+mk9N3fb5a6jrgV617Ob1Oqf691WPqdprb6t1HMBSYTzD3J1mPKeGmItsTAlFZuUjzkyvN3QKBgQDWx52jTXvc4QyOqu4Fc0mPSDJEYBBB6XpnTXRIT8rUFD9SDTWRhb29UL2nrtoQ6/8ZTC+/wKQ5B9UAUEwNxSk8IsrWDhcuioxOc1rT7nPRoVXQarr35//NV1CPmZyn6ybpCqDTsy6mvtWLIJdjZCc+1yZevM1uvOtRMrWRhs8bZQKBgEqIpfu4JqVMvRtxUL9d7iQ/NW+4t2FN3rkwl8FaUGj0HEuaBdoMtgdVASp7ToBXZu0rL/twjzm9ZgibnYov3nqXbXBeT+h10oL9Wf7gS3MNMMXngYlzGeD6hsFyGzs9ir/niyHFIYY7Cg5kmV4pRZdpFyYcBzYJF+lnl11FaRm1AoGAFu1wMoKO+mE7ye8NQZ+w9o6qbwoiMicOXgCyrRV3fXQ73jJyyXoRayg3VrMfrDbFIJo1bq7N2Riw8DuiIsYtRLIiHP+cEefQWn+N7pnB21rxojICi3xEnlL30px/UJ2VpcLwsCisjjhI63UrM/z5A4hMHEjjVTLtm9lh8IsHiNECgYBf+YEmhG88617fUMa7p9VLcwZNXWOgAFX230CYBFQQIgDuwINASoM4GGVfnvLPym4+WNW3cvfI1sNSm3VXzoM+uSHZtQUkG0SjHZl3buNGXXaJ1iZK/EoRHGraukC6ZuaDsnnjFQbRqvdZIFXqEWFbPFSWBQm9g2dYSbzNu3u9Kg==";
+    private static final String isvPrivateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDChomOdGrRa/D889B9ARtsLb/9zVJxq7VPV9yTu6ZjbhtpwS8Qep/95W7pWcK9yvNH5pWPpYZYwRB55z2Z/9SVqn+2sVU18JCxsgw7o35FUg9Qu9eiZeRpCiV8fbjhI9ZJpDh4v48MKcYBRM0LBZSQjA67xh4K90PYxI7UAgnMWW25Ny37oYxmH8ZoihemWvaRPFx4k4c9knBL5aPEspwIQjfDztOcOoRMkhZo5hUuI/GsKlw0REZ5lUdGKzgQ5ec4ZBe4ijFTsEjSvHcLaJbVnw9PdQ7a80Gw0Cf3qwS6dOR9LgsPLoUsKf9XRu0X6csS5uqu4e4I8lJm+WNZb+l3AgMBAAECggEAFeSOCwjXfsiptND8mB0C85VgjsAddRVF+281hYZF2dnqCZMdk2Vhp/G686tPq2Gcfhfu9t2Xta8g22oRjklIfoGpDFbVSBP84kAvd+9/cMN6ssjj928v75HIme63sIBX3S43fCt+/iJIxRrJAuJhZTVGG+RWZus2Pmlnc704/L+qP93XOVwFk/hKXhy7/Aa2S7KjVr5SpEDNUJ94W8WEpFgfccCrlkbuLAWG9nJF1gAoi0w6AJfEsTWpnNjpdHfFDtcT9UdULgJuz9yhzZya+mYbcjmLATqFihXJWsw4AKPnjOyMRPZP3EdtmOdiRddRnj82dsN5pjz651xFk5EuvQKBgQDWScg70XXwXgm527Kytra81VHW/WKcOw1w37GUiLJG4324fJeupXJ2zt4bmILrLhPTJkDNxE/1roX7GR1tn523H9s29b9Dqwfr38MSjfvJ5yPlaXrOLzDD7myDhZHTqvqgV1/id0IJZ3BeXTVEwd8yjfwEuZHW5s8TilOLoIcXswKBgQDoY/WTLfNleJLHFG4iIDbYBdK7zx105WVOmZF893sEP/4XPVq4RakSZG3CpVpYH5bsXyPTjwUoCoLLXmJzSJFLISemkn4Ot8xJRWPgZoDqm1Fi72O6VgNJlu9lX2ZcMKH4pAXqe7WMBSrxZqXL52gZsyv5YM+uBkY29DtHp7zFLQKBgDn1juEPEHVJGhxZHgZUgSymDhK2SjuzhTkoZ+Gi74VY9qI1oNkuCr2zykNwhsiRl+8eg5ykInRzFe4KpvkFmST0ytgcs/Tbh7L2vM6B9L5xdDYSx5KJFQmJrXQNZpn3vv4rY9XfJ89fWPdNAqFsRrBn0uh8QMP9fbjtSxeS/bcdAoGBAJTJqymYegW1tQQRaJIg3fxhfhMRAGMfnEU+vY+tQ+3sqtpmRfdFYoKMGlpNVBKn5xFfuKhzIXIJiMR8obv98kiP6bsUf/EcbIddDh1Wg6Ox3eHiM4/SEjjDknLtKbRMzudK3R7MJeiIRn5Yoj5y4ovR043PFijti3cT2ACAvLPhAoGAX5zTGz4M/JuqF5TWLKARGAZ9HiYoTLtJs5l/yTkKR4NO/E79TsBlUfDBhVYb+A3ChN6P8JX3cSwUplSiUo12eqG9/DPUSmeRrhTXlbul7metzaEVl1fQhaOIHflxkvg+FZIIt+NHyi6oGmDPXYfwUU3QqifMP5mF+v7IjTcPyD4=";
     private static final String yopPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4g7dPL+CBeuzFmARI2GFjZpKODUROaMG+E6wdNfv5lhPqC3jjTIeljWU8AiruZLGRhl92QWcTjb3XonjaV6k9rf9adQtyv2FLS7bl2Vz2WgjJ0FJ5/qMaoXaT+oAgWFk2GypyvoIZsscsGpUStm6BxpWZpbPrGJR0N95un/130cQI9VCmfvgkkCaXt7TU1BbiYzkc8MDpLScGm/GUCB2wB5PclvOxvf5BR/zNVYywTEFmw2Jo0hIPPSWB5Yyf2mx950Fx8da56co/FxLdMwkDOO51Qg3fbaExQDVzTm8Odi++wVJEP1y34tlmpwFUVbAKIEbyyELmi/2S6GG0j9vNwIDAQAB";
 
     private static final String serverRoot = "https://sandbox.yeepay.com/yop-center";
     private static final String apiUri = "/rest/v1.0/test/old-api-mgr/find-api-by-uri";
+    private static final String jsonApiUri = "/rest/v1.0/test-wdc/test/http-json/test";
     private static final String SLASH = "/";
     private static final String UNDER_LINE = "_";
     private static final String SEMICOLON = ";";
@@ -75,10 +79,12 @@ public class YopRsaEncryptExample {
 
     public static void main(String[] args) throws Exception {
         postFormExample();
+        postJsonExample();
         // TODO getFormExample
     }
 
     private static void postFormExample() throws Exception {
+        final HttpMethodName post = HttpMethodName.POST;
         // 请求加密
         String aesKey = encodeUrlSafeBase64(generateRandomKey());
         Set<String> encryptHeaders = Collections.emptySet();
@@ -108,11 +114,68 @@ public class YopRsaEncryptExample {
         headers.put(YOP_CONTENT_SHA256, calculateContentHash(params));
 
         // 签名头
-        headers.put(AUTHORIZATION, signFormRequest(headers, params));
+        headers.put(AUTHORIZATION, signRequest(post, apiUri, headers, params));
 
         // 构造请求
-        HttpUriRequest request = buildFormHttpRequest(serverRoot + apiUri,
-                HttpMethodName.POST, headers, params);
+        HttpUriRequest request = buildHttpRequest(serverRoot + apiUri,
+                post, headers, params, null);
+
+        // 发起请求
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(request);
+            handleResponse(YopRequestType.WEB, response);
+        } finally {
+            HttpClientUtils.closeQuietly(response);
+        }
+    }
+
+    private static void postJsonExample() throws Exception {
+        final HttpMethodName post = HttpMethodName.POST;
+        // 请求加密
+        String aesKey = encodeUrlSafeBase64(generateRandomKey());
+        Set<String> encryptHeaders = Collections.emptySet();
+        Set<String> encryptParams = Sets.newHashSet();
+
+        String plainJsonContent = ("{\n" +
+                "  \"arg1\" : {\n" +
+                "    \"appId\" : \"app_1111111111\",\n" +
+                "    \"customerNo\" : \"333333333\"\n" +
+                "  },\n" +
+                "  \"arg0\" : {\n" +
+                "    \"string\" : \"hello\",\n" +
+                "    \"array\" : [ \"test\" ]\n" +
+                "  }\n" +
+                "}"),
+                encryptJsonContent = encryptParam(aesKey, plainJsonContent);
+        String finalJsonContent
+        // 不加密
+//                = plainJsonContent;
+        // 加密
+              = encryptJsonContent;
+        encryptParams.add("$");
+
+        // 请求头
+        Map<String, String> headers = new HashMap<>();
+        headers.put(YOP_SDK_LANGS, "java");
+        headers.put(YOP_SDK_VERSION, "4.3.0");
+        headers.put(YOP_APPKEY, appKey);
+        headers.put(YOP_REQUEST_ID, UUID.randomUUID().toString());
+
+        // 加密头：请求参数不加密的情况, 也需设置加密头，用于响应结果加密
+        headers.put(YOP_ENCRYPT,buildEncryptHeader(encryptHeaders, encryptParams, aesKey));
+
+        // 摘要头
+        headers.put(YOP_CONTENT_SHA256, calculateContentHash(finalJsonContent));
+
+        // 签名头
+        // 一般json接口不会再有form参数, 留空即可
+        final ArrayListMultimap<String, String> params = ArrayListMultimap.create();
+        headers.put(AUTHORIZATION, signRequest(post, jsonApiUri, headers, params));
+
+        // 构造请求
+        HttpUriRequest request = buildHttpRequest(serverRoot + jsonApiUri,
+                post, headers, params, finalJsonContent);
 
         // 发起请求
         CloseableHttpResponse response = null;
@@ -136,6 +199,7 @@ public class YopRsaEncryptExample {
     private static final String YOP_ENCRYPT = "x-yop-encrypt";
     private static final String AUTHORIZATION = "Authorization";
     private static final String CONTENT_DISPOSITION = "Content-Disposition";
+    private static final String CONTENT_TYPE = "Content-Type";
 
     static {
         DEFAULT_HEADERS_TO_SIGN.add(YOP_REQUEST_ID);
@@ -144,13 +208,13 @@ public class YopRsaEncryptExample {
         DEFAULT_HEADERS_TO_SIGN.add(YOP_ENCRYPT);
     }
 
-    private static String signFormRequest(Map<String, String> headers, Multimap<String, String> params) throws Exception {
+    private static String signRequest(HttpMethodName httpMethod, String apiUri, Map<String, String> headers, Multimap<String, String> params) throws Exception {
         // A.构造认证字符串
         String authString = buildAuthString();
 
         // B.获取规范请求串
         SortedMap<String, String> headersToSign = getHeadersToSign(headers, DEFAULT_HEADERS_TO_SIGN);
-        String canonicalRequest = buildCanonicalRequest(authString, headersToSign);
+        String canonicalRequest = buildCanonicalRequest(httpMethod, apiUri, authString, headersToSign);
 
         // C.计算签名
         String signature = encodeUrlSafeBase64(sign(canonicalRequest.getBytes(DEFAULT_ENCODING))) + "$" + "SHA256";
@@ -191,14 +255,14 @@ public class YopRsaEncryptExample {
         return Base64.decodeBase64(input);
     }
 
-    private static String buildCanonicalRequest(String authString,
-                                         SortedMap<String, String> headersToSign) {
-        String canonicalQueryString = "";
+    private static String buildCanonicalRequest(HttpMethodName httpMethod, String apiUri, String authString,
+                                                SortedMap<String, String> headersToSign) {
+        String canonicalQueryString = "";// post from 与json时，均为空，此处先简单处理
         String canonicalHeaders = getCanonicalHeaders(headersToSign);
 
         String canonicalURI = getCanonicalURIPath(apiUri);
         return authString + "\n"
-                + "POST" + "\n"
+                + httpMethod.name() + "\n"
                 + canonicalURI + "\n"
                 + canonicalQueryString + "\n"
                 + canonicalHeaders;
@@ -277,16 +341,23 @@ public class YopRsaEncryptExample {
         return Hex.encodeHexString((digest(contentStream)));
     }
 
+    private static String calculateContentHash(String jsonContent) throws Exception {
+        InputStream contentStream = getContentStream(jsonContent);
+        return Hex.encodeHexString((digest(contentStream)));
+    }
+
     private static ByteArrayInputStream getContentStream(Multimap<String, String> params) throws Exception {
-        String encodedParameters = getCanonicalQueryString(params.asMap(), true);
+        return getContentStream(getCanonicalQueryString(params.asMap(), true));
+    }
+
+    private static ByteArrayInputStream getContentStream(String paramStr) throws Exception {
         byte[] bytes;
-        if (StringUtils.isEmpty(encodedParameters)) {
+        if (StringUtils.isEmpty(paramStr)) {
             bytes = new byte[0];
         } else {
-            bytes = encodedParameters.getBytes(DEFAULT_ENCODING);
+            bytes = paramStr.getBytes(DEFAULT_ENCODING);
         }
         return new ByteArrayInputStream(bytes);
-
     }
 
     public static String getCanonicalQueryString(Map<String, Collection<String>> parameters, boolean forSignature) {
@@ -446,8 +517,9 @@ public class YopRsaEncryptExample {
         return generator.generateKey().getEncoded();
     }
 
-    protected static HttpUriRequest buildFormHttpRequest(String requestUrl, HttpMethodName requestMethod,
-                                                         Map<String, String> headers, Multimap<String, String> params) {
+    protected static HttpUriRequest buildHttpRequest(String requestUrl, HttpMethodName requestMethod,
+                                                     Map<String, String> headers, Multimap<String, String> params,
+                                                     String entityString) throws UnsupportedEncodingException {
         RequestBuilder requestBuilder;
         if (HttpMethodName.POST == requestMethod) {
             requestBuilder = RequestBuilder.post();
@@ -465,14 +537,22 @@ public class YopRsaEncryptExample {
 
         // body
         try {
-            for (Map.Entry<String, Collection<String>> entry : params.asMap().entrySet()) {
-                String paramKey = entry.getKey();
-                for (String value : entry.getValue()) {
-                    requestBuilder.addParameter(paramKey, URLEncoder.encode(value, "UTF-8"));
+            if (null != params) {
+                for (Map.Entry<String, Collection<String>> entry : params.asMap().entrySet()) {
+                    String paramKey = entry.getKey();
+                    for (String value : entry.getValue()) {
+                        requestBuilder.addParameter(paramKey, URLEncoder.encode(value, DEFAULT_ENCODING));
+                    }
                 }
             }
         } catch (IOException ex) {
             throw new RuntimeException("unable to create http request.", ex);
+        }
+        // json 请求
+        if (StringUtils.isNotBlank(entityString)) {
+            final byte[] contentBytes = entityString.getBytes(DEFAULT_ENCODING);
+            requestBuilder.setEntity(new InputStreamEntity(new ByteArrayInputStream(contentBytes), contentBytes.length));
+            requestBuilder.addHeader(CONTENT_TYPE, "application/json;charset=UTF-8");
         }
         return requestBuilder.build();
     }
