bc4f513a5debe000722b5a7a1a9aa6cc84d62655
wdc <dachong.wang@yeepay.com>
2024-06-27 16:40:17 +0800

    fix: 商户通知，跨环境示例

--- yop-java-sdk-test/src/test/java/com/yeepay/yop/sdk/service/common/YopCallbackEngineTest.java
+++ yop-java-sdk-test/src/test/java/com/yeepay/yop/sdk/service/common/YopCallbackEngineTest.java
@@ -42,7 +42,6 @@ public class YopCallbackEngineTest extends BaseTest {
 
     @Before
     public void init() {
-        System.setProperty("yop.sdk.config.env", "qa");
         System.setProperty("yop.sdk.config.file", "yop_sdk_config_test_callback.json");
         MockYopCallbackHandler mockHandler = new MockYopCallbackHandler();
         YopCallbackHandlerFactory.register(mockHandler.getType(), mockHandler);
@@ -52,6 +51,8 @@ public class YopCallbackEngineTest extends BaseTest {
     public void testRsa() {
         YopCallbackRequest request =
                 new YopCallbackRequest(callback,"POST")
+                        .setProvider("yeepay")
+                        .setEnv("qa")
                         .setContentType(YopContentType.FORM_URL_ENCODE)
                         .addParam("customerIdentification", "app_Fe51qCyZWcEnDMtK")
                         .addParam("response", "ZIcrArlonH0mxIWCRejL2VQS2qeK5EFz2gALzdMbusIU8eqwnWNgJRWiTwJElSQEhnT42KkU3jXWZr2dd0A8-bZSjT-hvNCUI0aoJZkadRtJrWoe_ygGhOLegj7cTbk8y7GOzfFQteIFbB9ALae1CqWVHgfgyozbTLgsse4MfuYjio9r3DOkCJJSkW6mEHB0G4rTXSWFni0h_Uhtu5jsuCTU4vWDPKrBIZI17rr1AIqmyOd8C8oLCAplC1JT4KnLq5QCir4cnvZJrGYB5-bI00gPOdGX2_v4Az3VqMkh8PqqPSDriJ-PqDo9T2dnjR5njYkTSSzUpIXg6cfhLaTNIQ$0sn1fsX0zRYXv-bb0tV531Brbhb-fPORrXYqe8JzHbnL8NkAwIPRkSaTXfq3etJnmslkkBlPpZeTc7639TWQlBrl3eVW-aQKIjFX4bhfyythIh5ByjBAHw1RaYwoHw10kkpbBBk01K-6pzE9QzT6TvjZLsSsXZ6O3WJdvrB8dtpJA-PI-sOzm7DXkBqfKOSufkN1C1mRvexBlcN3ScSH2TKo5ZwKw3Fo_93GsYFD0hzYmHpC6yCyHXeY1PPlHYqd_KsqXVo_xBtXMCadoKnldYnMljXdhAQJLRdlkwTgeD8FX18SQSJ18O6Ag0w3IM9QXkcgZVgIo1-_ZUncc5AtNXyQCvfT4tNyaIRFsXlFqj5tCc5bekMz8OzYeRTPfmfCLKXmjvg4ICMw0aIRboX1tyZpCHdHU269u0-wX90pMDNRqBZsLag6glNDSzEG8RQaB4vGrjvxYy0ixeUnogwni2qqnnGX5Gfhkst7FPYubAsi5HweDT_aJIrmE6kMiBrpMAOcIGZ6slYK854FOH3ODO9-raz7n2P__NUTpziTF4t4Jru_erJevVoGyHH81qq_msIMvK7IRx2z1QoExRL08A$AES$SHA256");
@@ -64,6 +65,7 @@ public class YopCallbackEngineTest extends BaseTest {
     public void testSm4() {
         YopCallbackRequest request =
                 new YopCallbackRequest(callback,"POST")
+                        .setProvider("yeepay").setEnv("qa")
                         .setContentType(YopContentType.FORM_URL_ENCODE)
                         .addParam("customerIdentification", "app_15958159879157110009")
                         .addParam("cipherText", "2MV2vk_W8NlpHxB3L8wdiBXVALVF13aNxmbwILLQiSNdWYSXN_aM2YZk1n-oPMdW-heEt9_cIyNb")
@@ -85,6 +87,7 @@ public class YopCallbackEngineTest extends BaseTest {
         headers.put("Content-Type", "application/json");
         YopCallbackRequest request =
                 new YopCallbackRequest(callback, "POST")
+                        .setProvider("yeepay").setEnv("qa")
                         .setContentType(YopContentType.JSON)
                         .setHeaders(headers)
                         .setContent("EZgjreIx_ZW-gIM2NtHoKSk2sMQ35eolEjZ76XPcCtEqbXRfv77Z2eUJHhfoN4TcAZjPykzzDJ2pH7FC8xbhXw");
