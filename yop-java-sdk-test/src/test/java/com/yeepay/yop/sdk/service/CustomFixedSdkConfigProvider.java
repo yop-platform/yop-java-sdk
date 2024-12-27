6e688b7e308a11f50a54265077aa8dd7016936f8
wdc <dachong.wang@yeepay.com>
2024-12-27 10:23:34 +0800

    feat：sdk额外配置示例

--- yop-java-sdk-test/src/test/java/com/yeepay/yop/sdk/service/CustomFixedSdkConfigProvider.java
+++ yop-java-sdk-test/src/test/java/com/yeepay/yop/sdk/service/CustomFixedSdkConfigProvider.java
@@ -4,9 +4,11 @@
  */
 package com.yeepay.yop.sdk.service;
 
-import com.yeepay.yop.sdk.YopConstants;
 import com.yeepay.yop.sdk.base.config.provider.YopFixedSdkConfigProvider;
 import com.yeepay.yop.sdk.config.YopSdkConfig;
+import com.yeepay.yop.sdk.config.provider.file.YopProxyConfig;
+
+import java.util.Arrays;
 
 /**
  * title: 自定义SDK配置<br>
@@ -23,10 +25,20 @@ public class CustomFixedSdkConfigProvider extends YopFixedSdkConfigProvider {
     @Override
     protected YopSdkConfig loadSdkConfig() {
         YopSdkConfig yopSdkConfig = new YopSdkConfig();
-        // 可以根据当前环境(测试/灰度/生产等等)，自定义其他请求根路径
-        yopSdkConfig.setServerRoot(YopConstants.DEFAULT_SERVER_ROOT);
-        yopSdkConfig.setYosServerRoot(YopConstants.DEFAULT_YOS_SERVER_ROOT);
-        yopSdkConfig.setSandboxServerRoot(YopConstants.DEFAULT_SANDBOX_SERVER_ROOT);
+
+        // 示例：普通接口请求端点地址，配置多个时，遇到故障可以自动切换
+        yopSdkConfig.setPreferredServerRoots(Arrays.asList("https://域名A","https://域名B"));
+        // 示例：文件上传、下载类接口请求端点地址
+        yopSdkConfig.setYosServerRoot("https://域名C");
+        // 示例：沙箱环境请求地址
+        yopSdkConfig.setSandboxServerRoot("https://域名D");
+
+        // 示例：请求代理配置
+        final YopProxyConfig proxyConfig = new YopProxyConfig();
+        proxyConfig.setHost("xxx");
+        proxyConfig.setPort(1234);
+        yopSdkConfig.setProxy(proxyConfig);
+
         // 连接超时时间、读取超时时间等其他配置，可根据需要setXXX即可
         return yopSdkConfig;
     }
